a7bb06ff4f5514707d41bf7c4dfbb2f04c39c197
Anbarasu Chinnaswamy <anbarasu.chinnaswamy@synchronoss.com>
2017-08-30 15:22:51 -0400

    Initial Commit

---
+++ src/test/scala/Engine.scala
@@ -0,0 +1,18 @@
+import io.gatling.app.Gatling
+import io.gatling.core.config.GatlingPropertiesBuilder
+
+object Engine extends App {
+
+	val props = new GatlingPropertiesBuilder()
+		.dataDirectory(IDEPathHelper.dataDirectory.toString)
+		.resultsDirectory(IDEPathHelper.resultsDirectory.toString)
+		.bodiesDirectory(IDEPathHelper.bodiesDirectory.toString)
+		.binariesDirectory(IDEPathHelper.mavenBinariesDirectory.toString)
+	  .runDescription("N/A")
+    .simulationClass("com.eztest.restapi.GatlingSmoketest")
+//    .simulationClass("com.eztest.restapi.EchoTest")
+
+
+
+	Gatling.fromMap(props.build)
+}
